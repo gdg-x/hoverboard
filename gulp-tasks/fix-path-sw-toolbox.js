81cc1a4dbb8764effc957e042ece864f3cdbd077
Josef Jezek <josef@jezek.co.cz>
2015-08-25 12:22:27 +0200

    Fix path in fix-path-sw-toolbox task

--- gulp-tasks/fix-path-sw-toolbox.js
+++ gulp-tasks/fix-path-sw-toolbox.js
@@ -3,6 +3,6 @@
 // Fix path to sw-toolbox.js
 module.exports = function ($, gulp) { return function () {
   return gulp.src('dist/elements/bootstrap/sw-toolbox-setup.js')
-    .pipe($.replace('../sw-toolbox/sw-toolbox.js', '../../sw-toolbox/sw-toolbox.js'))
+    .pipe($.replace('"../sw-toolbox/sw-toolbox.js', '"../../sw-toolbox/sw-toolbox.js'))
     .pipe(gulp.dest('dist/elements/bootstrap'));
 };};
