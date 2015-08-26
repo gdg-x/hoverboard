a84d0bd2cf8024f268d838ff9a8c1bde19774295
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2015-08-27 00:45:24 +0300

    Replaced devfest season logo with devfest ukraine

--- gulpfile.js
+++ gulpfile.js
@@ -207,7 +207,7 @@ gulp.task('gh-pages', function() {
 });
 
 // Clean Output Directory
-gulp.task('clean', del.bind(null, ['.tmp', 'dist', '.publish']));
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 // Clear cache. Fixes problem with image task after moving project folder
 gulp.task('clearCache', function (done) {
