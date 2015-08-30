6512498fa514fa6a34d3ae28beafe60a99628442
Josef Jezek <josef@jezek.co.cz>
2015-08-30 23:48:20 +0200

    Improve download:fonts task

--- gulp-tasks/download-fonts.js
+++ gulp-tasks/download-fonts.js
@@ -4,6 +4,11 @@
 module.exports = function ($, gulp) { return function () {
   return gulp.src('./fonts.list')
     .pipe($.googleWebfonts())
-    .pipe(gulp.dest('app/fonts'))
-    .pipe($.size({title: 'app/fonts'}));
+    .pipe($.if('*.css', $.replace('url(', 'url(../fonts/')))
+    .pipe($.if('*.css', gulp.dest('app/styles')))
+    .pipe($.if('*.css', $.size({title: 'app/styles'})))
+    .pipe($.if('*.woff', gulp.dest('app/fonts')))
+    .pipe($.if('*.woff', $.size({title: 'app/fonts/*.woff'})))
+    .pipe($.if('*.woff2', gulp.dest('app/fonts')))
+    .pipe($.if('*.woff2', $.size({title: 'app/fonts/*.woff2'})));
 };};
