9f0178bf15a0f3340ccba36e22d495afb7b9e252
Josef Jezek <josef@jezek.co.cz>
2015-09-29 01:01:18 +0200

    Fix manifest task - improve gulp-file setup

--- tasks/manifest.js
+++ tasks/manifest.js
@@ -5,8 +5,11 @@ module.exports = function ($, config, gulp) { return function () {
   var variables = require('../app/themes/' + config.theme + '/variables');
   var manifest = require('../app/manifest');
 
-  return gulp.src('')
-    .pipe($.file('manifest.json', JSON.stringify(require('merge')(manifest, variables.manifest))))
+  return $.file(
+      'manifest.json',
+      JSON.stringify(require('merge')(manifest, variables.manifest)),
+      { src: true }
+    )
     .pipe(gulp.dest('.tmp'))
     .pipe(gulp.dest('dist'));
 };};
