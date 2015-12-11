aed9459b4b671fb255734d52ca33b944f65c50ff
Josef Jezek <josef@jezek.co.cz>
2015-12-11 13:46:39 +0100

    Fix charset

--- app/metadata.js
+++ app/metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Polymer Starter Kit Plus',
   description: '',
-  charset: 'utf-8',
+  charset: 'UTF-8',
   lang: 'en',
   // Add to homescreen for Chrome on Android
   applicationName: 'PSK Plus',
