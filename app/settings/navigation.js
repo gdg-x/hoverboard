01916bfdfa299e3251034a5de7d873e7a4da88c3
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2015-12-28 00:33:25 +0200

    Change iron-pages to neon-animated-pages

--- app/settings/navigation.js
+++ app/settings/navigation.js
@@ -9,15 +9,17 @@ module.exports = {
         permalink: '/blog',
         text: 'Blog',
         styleSettings: ['#03A9F4', '', '#FFFFFF', '#FFFFFF', '#FFFFFF']
-    }, {
-        route: 'schedule',
-        permalink: '/schedule',
-        text: 'Schedule',
-        styleSettings: ['#009688', '', '#FFFFFF', '#FFFFFF', '#FFFFFF']
-    }, {
-        route: 'speakers',
-        permalink: '/speakers',
-        text: 'Speakers',
-        styleSettings: ['#00BCD4', '', '#FFFFFF', '#FFFFFF', '#FFFFFF']
-    }]
+    },
+    //    {
+    //    route: 'schedule',
+    //    permalink: '/schedule',
+    //    text: 'Schedule',
+    //    styleSettings: ['#009688', '', '#FFFFFF', '#FFFFFF', '#FFFFFF']
+    //}, {
+    //    route: 'speakers',
+    //    permalink: '/speakers',
+    //    text: 'Speakers',
+    //    styleSettings: ['#00BCD4', '', '#FFFFFF', '#FFFFFF', '#FFFFFF']
+    //}
+    ]
 };
