d141177bf40565e75eb2042cd8937621c3d46b62
Josef Jezek <josef@jezek.co.cz>
2015-12-31 17:00:02 +0100

    Update config for stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
   "rules": {
     "color-no-invalid-hex": 2,
     "declaration-colon-space-before": [2, "never"],
     "function-comma-space-after": [2, "always"],
-    "function-url-quotes": [2, "double"],
+    "function-url-quotes": [2, "single"],
     "media-feature-colon-space-after": [2, "always"],
     "media-feature-colon-space-before": [2, "never"],
     "media-feature-name-no-vendor-prefix": 2,
@@ -15,7 +15,7 @@
     "rule-no-single-line": 2,
     "rule-trailing-semicolon": [2, "always"],
     "selector-list-comma-newline-after": [2, "always-multi-line"],
-    "string-quotes": [2, "double"],
+    "string-quotes": [2, "single"],
     "value-no-vendor-prefix": 2
   }
-}
+};
