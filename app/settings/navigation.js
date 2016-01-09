9b2b6e15511026b38345a97141076b028fb9a78f
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2016-01-09 15:32:20 +0200

    Change a way to add title to the page

--- app/settings/navigation.js
+++ app/settings/navigation.js
@@ -4,11 +4,9 @@ module.exports = {
         permalink: '/',
         text: 'Home',
         heroSettings: {
-            title: 'GDG DevFest Ukraine 2015',
             backgroundColor: '#FFFFFF',
             backgroundImage: '',
-            fontColor: '#212121',
-            tabFontColor: '#00BCD4',
+            fontColor: '#00BCD4',
             tabBarColor: '#00BCD4'
         }
     }, {
@@ -16,11 +14,9 @@ module.exports = {
         permalink: '/blog',
         text: 'Blog',
         heroSettings: {
-            title: 'Blog',
             backgroundColor: '#03A9F4',
             backgroundImage: '/images/backgrounds/blog.png',
             fontColor: '#FFFFFF',
-            tabFontColor: '#FFFFFF',
             tabBarColor: '#FFFFFF'
         }
     }]
