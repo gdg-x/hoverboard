6b3fdc9ce4fea4d04f33c0b9246156518613e2f9
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2016-03-03 01:15:29 +0200

    Fix path to scripts. Fix dialog opening

--- tasks/fix-paths.js
+++ tasks/fix-paths.js
@@ -8,7 +8,9 @@ module.exports = function ($, gulp, merge, order) { return function () {
     baseBundle = gulp.src('dist/elements/base-bundle.js')
       .pipe($.replace('("bootstrap/', '("/bower_components/platinum-sw/bootstrap/'))
       .pipe($.replace("('bootstrap/", "('/bower_components/platinum-sw/bootstrap/"))
-      .pipe($.replace('scripts/analytics.js', '/scripts/analytics.js'))
+      .pipe($.replace("('/bootstrap/", "('/bower_components/platinum-sw/bootstrap/"))
+      .pipe($.replace("'scripts/analytics.js'", "'/scripts/analytics.js'"))
+      .pipe($.replace("'sw-import.js'", "'/sw-import.js'"))
       .pipe(gulp.dest('dist/elements'));
 
     swToolboxSetup = gulp.src('dist/bower_components/platinum-sw/bootstrap/sw-toolbox-setup.js')
@@ -18,7 +20,6 @@ module.exports = function ($, gulp, merge, order) { return function () {
   } else if (order === 'after') {
     baseBundle = gulp.src('deploy/elements/base-bundle.*.js')
       .pipe($.replace('("../bower_components/platinum-sw/bootstrap/', '("/bower_components/platinum-sw/bootstrap/'))
-      .pipe($.replace('scripts/analytics.js', '/scripts/analytics.js'))
       .pipe(gulp.dest('deploy/elements'));
 
     swToolboxSetup = gulp.src('deploy/bower_components/platinum-sw/bootstrap/sw-toolbox-setup.*.js')
