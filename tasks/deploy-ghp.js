23ca9db6b839f358f877f632080edbc7489cc33b
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2016-03-14 01:35:11 +0200

    Prepare to release

--- tasks/deploy-ghp.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var ghPages = require('gulp-gh-pages');
-
-// Deploy to GitHub pages
-module.exports = function ($, config, gulp, environment) {
-
-  return gulp.src('./dist/**/*').pipe(ghPages());
-};
