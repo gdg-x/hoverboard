93c60be7dd3a5590b3cc9de93e84cf6b21ef7ae8
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2017-01-13 19:16:34 +0200

    Update to ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,170 +1,270 @@
-'use strict';
-
-const path = require('path');
-const gulp = require('gulp');
-const gulpif = require('gulp-if');
-const uglify = require('gulp-uglify');
-const cssSlam = require('css-slam').gulp;
-const htmlmin = require('gulp-htmlmin');
-const browserSync = require('browser-sync').create();
-const history = require('connect-history-api-fallback');
-const requireUncached = require('require-uncached');
-
-// Got problems? Try logging 'em
+// 'use strict';
+//
+// const path = require('path');
+// const gulp = require('gulp');
+// const gulpif = require('gulp-if');
+// const uglify = require('gulp-uglify');
+// const cssSlam = require('css-slam').gulp;
+// const htmlmin = require('gulp-htmlmin');
+// const browserSync = require('browser-sync').create();
+// const history = require('connect-history-api-fallback');
+// const requireUncached = require('require-uncached');
+//
+// Got
+// problems ? Try logging
+// 'em
 // const logging = require('plylog');
 // logging.setVerbose();
+//
+//
+// global.config = {
+//   polymerJsonPath: path.join(process.cwd(), 'polymer.json'),
+//   build: {
+//     rootDirectory: 'build',
+//     bundledDirectory: 'bundled',
+//     unbundledDirectory: 'unbundled',
+//     // Accepts either 'bundled', 'unbundled', or 'both'
+//     // A bundled version will be vulcanized and sharded. An unbundled version
+//     // will not have its files combined (this is for projects using HTTP/2
+//     // server push). Using the 'both' option will create two output projects,
+//     // one for bundled and one for unbundled
+//     bundleType: 'both'
+//   },
+//   // Path to your service worker, relative to the build root directory
+//   serviceWorkerPath: 'service-worker.js',
+//   // Service Worker precache options based on
+//   // https://github.com/GoogleChrome/sw-precache#options-parameter
+//   swPrecacheConfig: {
+//     staticFileGlobs: [
+//       '/index.html',
+//       '/manifest.json',
+//       '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+//       '/src/**/*',
+//       '/scripts/**/*',
+//       '/images/**/*',
+//       '/data/**/*'
+//     ],
+//     navigateFallback: '/index.html',
+//     navigateFallbackWhitelist: [/^\/[^\_]+\//]
+//   },
+//   templatePath: [
+//     'scripts/**/*.js',
+//     'src/**/*.html',
+//     'index.html',
+//     'manifest.json'
+//   ]
+// };
+//
+// const clean = require('./gulp-tasks/clean.js');
+// const images = require('./gulp-tasks/images.js');
+// const template = require('./gulp-tasks/template.js');
+// const project = require('./gulp-tasks/project.js');
+//
+// // The source task will split all of your source files into one
+// // big ReadableStream. Source files are those in src/** as well as anything
+// // added to the sourceGlobs property of polymer.json.
+// // Because most HTML Imports contain inline CSS and JS, those inline resources
+// // will be split out into temporary files. You can use gulpif to filter files
+// // out of the stream and run them through specific tasks. An example is provided
+// // which filters all images and runs them through imagemin
+// function source() {
+//   const metadata = requireUncached('./data/hoverboard.config');
+//   const resources = requireUncached('./data/en/resources');
+//   return project.sources()
+//     .pipe(gulpif('**/*.{html,js,json}', template.compile(Object.assign({}, metadata, resources))))
+//     .pipe(project.splitHtml())
+//     // Add your own build tasks here!
+//     // splitHtml doesn't split CSS https://github.com/Polymer/polymer-build/issues/32
+//     .pipe(gulpif(/\.js$/, uglify()))
+//     .pipe(gulpif('**/*.{html,css}', cssSlam()))
+//     .pipe(gulpif(/\.html$/, htmlmin({
+//       caseSensitive: true,
+//       collapseWhitespace: true,
+//       collapseInlineTagWhitespace: true,
+//       removeComments: true,
+//       removeCommentsFromCDATA: true,
+//       customAttrAssign: [/\$=/],
+//       customAttrSurround: [
+//         [{'source': '\\({\\{'}, {'source': '\\}\\}'}],
+//         [{'source': '\\[\\['}, {'source': '\\]\\]'}]
+//       ]
+//     })))
+//     .pipe(gulpif('**/*.{png,gif,jpg,svg}', images.minify()))
+//     .pipe(project.rejoin()); // Call rejoin when you're finished
+// }
+//
+// // The dependencies task will split all of your bower_components files into one
+// // big ReadableStream
+// // You probably don't need to do anything to your dependencies but it's here in
+// // case you need it :)
+// function dependencies() {
+//   return project.splitDependencies()
+//   // Doesn't work for now
+//   // .pipe(gulpif(/\.js$/, uglify()))
+//     .pipe(gulpif('**/*.{html,css}', cssSlam()))
+//     .pipe(gulpif(/\.html$/, htmlmin({
+//       caseSensitive: true,
+//       collapseWhitespace: true,
+//       collapseInlineTagWhitespace: true,
+//       removeComments: true,
+//       removeCommentsFromCDATA: true,
+//       customAttrAssign: [/\$=/],
+//       customAttrSurround: [
+//         [{'source': '\\({\\{'}, {'source': '\\}\\}'}],
+//         [{'source': '\\[\\['}, {'source': '\\]\\]'}]
+//       ]
+//     })))
+//     .pipe(project.rejoin());
+// }
+//
+// function compileTemplate() {
+//   const metadata = requireUncached('./data/hoverboard.config');
+//   const resources = requireUncached('./data/en/resources');
+//   return gulp.src(global.config.templatePath, {base: '.'})
+//     .pipe(template.compile(Object.assign({}, metadata, resources)));
+// }
+//
+// gulp.task('template', gulp.series(clean('.temp'), () => {
+//   return compileTemplate()
+//     .pipe(gulp.dest('.temp'));
+// }));
+//
+// function reload(done) {
+//   browserSync.reload();
+//   done();
+// }
+//
+// // Clean the build directory, split all source and dependency files into streams
+// // and process them, and output bundled and unbundled versions of the project
+// // with their own service workers
+// gulp.task('default', gulp.series([
+//   clean([global.config.build.rootDirectory]),
+//   project.merge(source, dependencies),
+//   project.serviceWorker
+// ]));
+//
+// gulp.task('serve', gulp.series('template', () => {
+//   browserSync.init({
+//     logPrefix: 'Hoverboard',
+//     notify: false,
+//     server: {
+//       baseDir: ['.temp', './'],
+//       middleware: [history()]
+//     }
+//   });
+//
+//   gulp.watch([
+//     'data/**/*.{markdown,md}',
+//     'images/**/*.{png,gif,jpg,svg}',
+//   ]).on('change', reload);
+//
+//   gulp.watch([
+//     'data/**/*.json',
+//     'scripts/**/*.js',
+//     'src/**/*.html',
+//     './index.html',
+//     'manifest.json'
+//   ], gulp.series('template', reload));
+// }));
 
-// !!! IMPORTANT !!! //
-// Keep the global.config above any of the gulp-tasks that depend on it
-global.config = {
-  polymerJsonPath: path.join(process.cwd(), 'polymer.json'),
+
+const del = require('del');
+const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const imagemin = require('gulp-imagemin');
+const mergeStream = require('merge-stream');
+const polymerBuild = require('polymer-build');
+const requireUncached = require('require-uncached');
+
+const config = {
+  polymerJsonPath: './polymer.json',
   build: {
     rootDirectory: 'build',
-    bundledDirectory: 'bundled',
-    unbundledDirectory: 'unbundled',
-    // Accepts either 'bundled', 'unbundled', or 'both'
-    // A bundled version will be vulcanized and sharded. An unbundled version
-    // will not have its files combined (this is for projects using HTTP/2
-    // server push). Using the 'both' option will create two output projects,
-    // one for bundled and one for unbundled
-    bundleType: 'both'
+    bundleType: 'bundled'
   },
-  // Path to your service worker, relative to the build root directory
-  serviceWorkerPath: 'service-worker.js',
-  // Service Worker precache options based on
-  // https://github.com/GoogleChrome/sw-precache#options-parameter
-  swPrecacheConfig: {
-    staticFileGlobs: [
-      '/index.html',
-      '/manifest.json',
-      '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-      '/src/**/*',
-      '/scripts/**/*',
-      '/images/**/*',
-      '/data/**/*'
-    ],
-    navigateFallback: '/index.html',
-    navigateFallbackWhitelist: [ /^\/[^\_]+\// ]
-  },
-  templatePath: [
-    'scripts/**/*.js',
-    'src/**/*.html',
-    'index.html',
-    'manifest.json'
-  ]
+  serviceWorkerPath: './service-worker.js',
+  swPrecacheConfigPath: './sw-precache-config.js',
+  templateData: [
+    './data/hoverboard.config',
+    './data/en/resources'
+  ],
+  tempDirectory: '.temp'
 };
-
-// Add your own custom gulp tasks to the gulp-tasks directory
-// A few sample tasks are provided for you
-// A task should return either a WriteableStream or a Promise
-const clean = require('./gulp-tasks/clean.js');
-const images = require('./gulp-tasks/images.js');
+const swPrecacheConfig = require(config.swPrecacheConfigPath);
+const polymerJson = require(config.polymerJsonPath);
+const buildPolymerJson = {
+  entrypoint: `${config.tempDirectory}/${polymerJson.entrypoint}`,
+  shell: `${config.tempDirectory}/${polymerJson.shell}`,
+  fragments: polymerJson.fragments.reduce((res, el) => [...res, `${config.tempDirectory}/${el}`], []),
+  sources: polymerJson.sources.reduce((res, el) => [...res, `${config.tempDirectory}/${el}`], []),
+  extraDependencies: polymerJson.extraDependencies
+};
 const template = require('./gulp-tasks/template.js');
-const project = require('./gulp-tasks/project.js');
 
-// The source task will split all of your source files into one
-// big ReadableStream. Source files are those in src/** as well as anything
-// added to the sourceGlobs property of polymer.json.
-// Because most HTML Imports contain inline CSS and JS, those inline resources
-// will be split out into temporary files. You can use gulpif to filter files
-// out of the stream and run them through specific tasks. An example is provided
-// which filters all images and runs them through imagemin
-function source() {
-  const metadata = requireUncached('./data/hoverboard.config');
-  const resources = requireUncached('./data/en/resources');
-  return project.sources()
-    .pipe(gulpif('**/*.{html,js,json}', template.compile(Object.assign({}, metadata, resources))))
-    .pipe(project.splitHtml())
-    // Add your own build tasks here!
-    // splitHtml doesn't split CSS https://github.com/Polymer/polymer-build/issues/32
-    .pipe(gulpif(/\.js$/, uglify()))
-    .pipe(gulpif('**/*.{html,css}', cssSlam()))
-    .pipe(gulpif(/\.html$/, htmlmin({
-      caseSensitive: true,
-      collapseWhitespace: true,
-      collapseInlineTagWhitespace: true,
-      removeComments: true,
-      removeCommentsFromCDATA: true,
-      customAttrAssign: [/\$=/],
-      customAttrSurround: [
-        [ {'source': '\\({\\{'}, {'source': '\\}\\}'} ],
-        [ {'source': '\\[\\['}, {'source': '\\]\\]'}  ]
-      ]
-    })))
-    .pipe(gulpif('**/*.{png,gif,jpg,svg}', images.minify()))
-    .pipe(project.rejoin()); // Call rejoin when you're finished
-}
 
-// The dependencies task will split all of your bower_components files into one
-// big ReadableStream
-// You probably don't need to do anything to your dependencies but it's here in
-// case you need it :)
-function dependencies() {
-  return project.splitDependencies()
-    // Doesn't work for now
-    // .pipe(gulpif(/\.js$/, uglify()))
-    .pipe(gulpif('**/*.{html,css}', cssSlam()))
-    .pipe(gulpif(/\.html$/, htmlmin({
-      caseSensitive: true,
-      collapseWhitespace: true,
-      collapseInlineTagWhitespace: true,
-      removeComments: true,
-      removeCommentsFromCDATA: true,
-      customAttrAssign: [/\$=/],
-      customAttrSurround: [
-        [ {'source': '\\({\\{'}, {'source': '\\}\\}'} ],
-        [ {'source': '\\[\\['}, {'source': '\\]\\]'}  ]
-      ]
-    })))
-    .pipe(project.rejoin());
+function waitFor(stream) {
+  return new Promise((resolve, reject) => {
+    stream.on('end', resolve);
+    stream.on('error', reject);
+  });
 }
 
 function compileTemplate() {
-  const metadata = requireUncached('./data/hoverboard.config');
-  const resources = requireUncached('./data/en/resources');
-  return gulp.src(global.config.templatePath, {base: '.'})
-    .pipe(template.compile(Object.assign({}, metadata, resources)));
+  let metadata = {};
+  for (let file of config.templateData) {
+    metadata = Object.assign({}, metadata, requireUncached(file));
+  }
+  console.log(`Compiling...`);
+  return gulp.src([
+    ...polymerJson.sources,
+    polymerJson.entrypoint
+  ], {base: '.'})
+    .pipe(gulpif(/\.(html|js|json)$/, template.compile(metadata)))
+    .pipe(gulp.dest(config.tempDirectory));
 }
 
-gulp.task('template', gulp.series(clean('.temp'), () => {
-  return compileTemplate()
-    .pipe(gulp.dest('.temp'));
-}));
+function build() {
+  return new Promise((resolve, reject)=> {
+    console.log(`Deleting ${config.build.rootDirectory} and ${config.tempDirectory} directory...`);
+    del([config.build.rootDirectory])
+      // .then(compileTemplate)
+      .then(() => {
+        const polymerProject = new polymerBuild.PolymerProject(buildPolymerJson);
+        console.log(`Polymer building...`);
+        console.log(buildPolymerJson);
+        let sourcesStream = polymerProject.sources()
+          .pipe(polymerProject.splitHtml())
+          .pipe(polymerProject.rejoinHtml());
 
-function reload(done) {
-  browserSync.reload();
-  done();
-}
+        let dependenciesStream = polymerProject.dependencies()
+          .pipe(polymerProject.splitHtml())
+          .pipe(polymerProject.rejoinHtml());
 
-// Clean the build directory, split all source and dependency files into streams
-// and process them, and output bundled and unbundled versions of the project
-// with their own service workers
-gulp.task('default', gulp.series([
-  clean([global.config.build.rootDirectory]),
-  project.merge(source, dependencies),
-  project.serviceWorker
-]));
+        let buildStream = mergeStream(sourcesStream, dependenciesStream)
+          .once('data', () => {
+            console.log('Analyzing build dependencies...');
+          });
 
-gulp.task('serve', gulp.series('template', () => {
-  browserSync.init({
-    logPrefix: 'Hoverboard',
-    notify: false,
-    server: {
-      baseDir: ['.temp', './'],
-      middleware: [history()]
-    }
+        buildStream = buildStream.pipe(polymerProject.bundler);
+        buildStream = buildStream.pipe(gulp.dest(config.build.rootDirectory));
+        return waitFor(buildStream);
+      })
+      // .then(() => {
+      //   console.log('Generating the Service Worker...');
+      //   return polymerBuild.addServiceWorker({
+      //     project: polymerProject,
+      //     buildRoot: config.build.rootDirectory,
+      //     bundled: true,
+      //     swPrecacheConfig: swPrecacheConfig
+      //   });
+      // })
+      .then(() => {
+        console.log('Build complete!');
+        resolve();
+      });
   });
+}
 
-  gulp.watch([
-    'data/**/*.{markdown,md}',
-    'images/**/*.{png,gif,jpg,svg}',
-  ]).on('change', reload);
+gulp.task('default', build);
+gulp.task('compile', compileTemplate);
 
-  gulp.watch([
-    'data/**/*.json',
-    'scripts/**/*.js',
-    'src/**/*.html',
-    './index.html',
-    'manifest.json'
-  ], gulp.series('template', reload));
-}));
