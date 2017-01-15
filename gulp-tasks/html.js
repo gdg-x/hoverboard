3b944afb1948023bdc133d08872f07e23fdb137a
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2017-01-15 17:04:55 +0200

    Update Polymer build library and gulp tasks (#219)
    * Minor refactoring
    
    * Update polymer-build
    
    * Update to ES6
    
    * Fix build step
    
    * Fix serve task. Divided build task into microtasks

---
+++ gulp-tasks/html.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const htmlmin = require('gulp-htmlmin');
+
+function minify() {
+  return htmlmin({
+    caseSensitive: true,
+    collapseWhitespace: true,
+    collapseInlineTagWhitespace: true,
+    removeComments: true,
+    removeCommentsFromCDATA: true,
+    customAttrAssign: [/\$=/],
+    customAttrSurround: [
+      [{'source': '\\({\\{'}, {'source': '\\}\\}'}],
+      [{'source': '\\[\\['}, {'source': '\\]\\]'}]
+    ]
+  });
+}
+
+module.exports = {
+  minify
+};
