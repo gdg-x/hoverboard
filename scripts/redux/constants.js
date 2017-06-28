1029d6de4a1f50a3ba8f9faafe3157c07e4e6cf3
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2017-06-28 11:49:51 +0300

    Implement Tickets block (#265)
    * Implement tickets block
    
    * Remove unnecessary css block. Load firebase library asynchronously

--- scripts/redux/constants.js
+++ scripts/redux/constants.js
@@ -1,3 +1,7 @@
 const TOGGLE_DRAWER = 'TOGGLE_DRAWER';
+const SET_VIEWPORT_SIZE = 'SET_VIEWPORT_SIZE';
+// router
 const SET_ROUTE = 'SET_ROUTE';
 const SET_HERO = 'SET_HERO';
+// tickets
+const FETCH_TICKETS = 'FETCH_TICKETS';
--- scripts/redux/reducer.js
+++ scripts/redux/reducer.js
@@ -4,6 +4,10 @@ const uiReducer = (state = initialState.ui, action) => {
       return Object.assign({}, state, {
         isDrawerOpened: action.value || !state.isDrawerOpened
       });
+    case SET_VIEWPORT_SIZE:
+      return Object.assign({}, state, {
+        viewport: Object.assign({}, state.viewport, action.value)
+      });
     default:
       return state;
   }
@@ -18,11 +22,19 @@ const routeReducer = (state = initialState.route, action) => {
   }
 };
 
-
 const heroReducer = (state = initialState.hero, action) => {
   switch (action.type) {
     case SET_HERO:
       return action.hero;
+    default:
+      return state;
+  }
+};
+
+const ticketsReducer = (state = initialState.tickets, action) => {
+  switch (action.type) {
+    case FETCH_TICKETS:
+      return action.tickets;
     default:
       return state;
   }
