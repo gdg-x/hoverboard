bd9f7f874f0bf43b307ff52321818120c95e9fef
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2017-09-07 17:14:13 +0200

    Update firebase db rules

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,6 @@
 importScripts('bower_components/firebase/firebase-app.js');
 importScripts('bower_components/firebase/firebase-messaging.js');
-
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
 firebase.initializeApp({
   messagingSenderId: '{$ firebase.messagingSenderId $}'
 });
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler(payload => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: 'images/logo.svg'
-  };
-
-  return self.registration.showNotification(notificationTitle,
-      notificationOptions);
-});
+firebase.messaging();
