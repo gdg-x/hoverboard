0083b88d9efe8ba8ef5523f3a93109cbf5931d04
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2017-10-26 01:17:49 +0300

    Fix schedule generation for some cases

--- scripts/helper/schedule-webworker.js
+++ scripts/helper/schedule-webworker.js
@@ -6,15 +6,11 @@ function clone(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
-function findSpeakerById(id, speakerList) {
-  return speakerList.filter(function(s) {return s.id === +id;})[0];
-}
-
 function attachSessionAndSpeakersTogether(session, speakersRaw) {
   if (isDefined(session.speakers)) {
     for (var speakerIdx = 0; speakerIdx < session.speakers.length; speakerIdx++) {
-      if (!isDefined(session.speakers[speakerIdx].id)) {
-        session.speakers[speakerIdx] = findSpeakerById(session.speakers[speakerIdx], speakersRaw);
+      if (isDefined(session.speakers[speakerIdx]) && !isDefined(session.speakers[speakerIdx].id)) {
+        session.speakers[speakerIdx] = speakersRaw[session.speakers[speakerIdx]];
         var tempSession = clone(session);
         delete tempSession.speakers;
         if (isDefined(session.speakers[speakerIdx])) {
