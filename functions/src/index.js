a9dbaf9df7a9e802b22161c24bc04a59def1ca30
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2018-04-09 15:27:22 +0300

    Firebase functions update to v1 (#431)
    * Update saveUserData function
    
    * Update saveUserData function
    
    * Update sendGeneralNotification function
    
    * Update scheduleNotifications function
    
    * Update optimizeImages function
    
    * Update mailchimpSubscribe function

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,13 +1,13 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import admin from 'firebase-admin';
 
-import { saveUserData } from './users';
-import { sendGeneralNotification } from './notifications';
-import { scheduleNotifications } from './schedule-notifications';
-import { optimizeImages } from './optimize-images';
-import { mailchimpSubscribe } from './mailchimp-subscribe';
+import saveUserData from './save-user-data';
+import sendGeneralNotification from './notifications';
+import scheduleNotifications from './schedule-notifications';
+import optimizeImages from './optimize-images';
+import mailchimpSubscribe from './mailchimp-subscribe';
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
 export {
   saveUserData,
