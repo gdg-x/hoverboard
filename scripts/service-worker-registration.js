a35a803fa07a8c63d81f89ebda0818857a176ecc
Hugo Gresse <hugo.gresse@teads.tv>
2018-11-13 14:24:10 +0100

    Remove service worker aka unregister

--- scripts/service-worker-registration.js
+++ scripts/service-worker-registration.js
@@ -4,66 +4,33 @@
   // Ensure we only attempt to register the SW once.
   let isAlreadyRegistered = false;
 
-  const URL = 'service-worker.js';
-  const SCOPE = Polymer.rootPath;
-
   const register = () => {
-    if (!isAlreadyRegistered) {
-      isAlreadyRegistered = true;
+    if (isAlreadyRegistered) {
+      return;
+    }
 
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register(URL, {
-          scope: SCOPE,
-        })
-          .then((registration) => {
-            registration.onupdatefound = () => {
-              const installingWorker = registration.installing;
+    isAlreadyRegistered = true;
 
-              installingWorker.onstatechange = () => {
-                switch (installingWorker.state) {
-                  case 'installed':
-                    if (!navigator.serviceWorker.controller && toastActions) {
-                      toastActions.showToast({
-                        message: '{$ cachingComplete $}',
-                      });
-                    }
-                    break;
-                  case 'redundant':
-                    throw Error('The installing service worker became redundant.');
-                }
-              };
-            };
-          })
-          .catch((e) => {
-            // eslint-disable-next-line no-console
-            console.error('Service worker registration failed:', e);
-          });
-      }
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(
+        function (registrations) {
+          for (let registration of registrations) {
+            registration.unregister()
+              .then(function () {
+                return self.clients.matchAll();
+              })
+              .then(function (clients) {
+                clients.forEach( (client) => {
+                  if (client.url && 'navigate' in client) {
+                      client.navigate(client.url);
+                  }
+                });
+              });
+          }
+        }
+      );
     }
   };
-
-  if (navigator.serviceWorker && navigator.serviceWorker.controller) {
-    navigator.serviceWorker.controller.onstatechange = (event) => {
-      if (event.target.state === 'redundant') {
-        const tapHandler = () => {
-          window.location.reload();
-        };
-
-        if (toastActions) {
-          toastActions.showToast({
-            message: '{$ newVersionAvailable $}',
-            action: {
-              title: '{$ refresh $}',
-              callback: tapHandler,
-            },
-            duration: 0,
-          });
-        } else {
-          tapHandler(); // Force reload if user never was shown the toast.
-        }
-      }
-    };
-  }
 
   window.addEventListener('load', register);
 })();
