25253162838bcc6e483641afdb6a2b2d6bdbecae
Hugo Gresse <hugo.gresse@teads.tv>
2018-11-13 15:00:26 +0100

    Change service workers + add release yarn command

--- scripts/service-worker-registration.js
+++ scripts/service-worker-registration.js
@@ -15,17 +15,7 @@
       navigator.serviceWorker.getRegistrations().then(
         function (registrations) {
           for (let registration of registrations) {
-            registration.unregister()
-              .then(function () {
-                return self.clients.matchAll();
-              })
-              .then(function (clients) {
-                clients.forEach( (client) => {
-                  if (client.url && 'navigate' in client) {
-                      client.navigate(client.url);
-                  }
-                });
-              });
+            registration.unregister();
           }
         }
       );
