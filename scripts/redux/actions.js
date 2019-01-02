bd4bdb66bfa895bb68591bae8a10de767daa6e93
Joss Clifford-Frith <jossie.cliffordfrith@gmail.com>
2019-01-02 15:41:48 -0500

    country code as text imput field

--- scripts/redux/actions.js
+++ scripts/redux/actions.js
@@ -602,6 +602,7 @@ const subscribeActions = {
         email: data.email,
         firstName: data.firstFieldValue || '',
         lastName: data.secondFieldValue || '',
+        countryCode: data.thirdFieldValue || '',
       })
       .then(() => {
         dispatch({
