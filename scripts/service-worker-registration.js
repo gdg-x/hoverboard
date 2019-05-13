7807f169c61076756ca22880ae8442a362d99050
Abraham Williams <4braham@gmail.com>
2019-05-13 10:43:31 -0500

    Remove SW check

--- scripts/service-worker-registration.js
+++ scripts/service-worker-registration.js
@@ -1,44 +1,37 @@
 (function () {
   'use strict';
 
-  // Ensure we only attempt to register the SW once.
-  let isAlreadyRegistered = false;
-
   const URL = 'service-worker.js';
   const SCOPE = Polymer.rootPath;
 
   const register = () => {
-    if (!isAlreadyRegistered) {
-      isAlreadyRegistered = true;
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register(URL, {
+        scope: SCOPE,
+      })
+        .then((registration) => {
+          registration.onupdatefound = () => {
+            const installingWorker = registration.installing;
 
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register(URL, {
-          scope: SCOPE,
-        })
-          .then((registration) => {
-            registration.onupdatefound = () => {
-              const installingWorker = registration.installing;
-
-              installingWorker.onstatechange = () => {
-                switch (installingWorker.state) {
-                  case 'installed':
-                    if (!navigator.serviceWorker.controller && toastActions) {
-                      toastActions.showToast({
-                        message: '{$ cachingComplete $}',
-                      });
-                    }
-                    break;
-                  case 'redundant':
-                    throw Error('The installing service worker became redundant.');
-                }
-              };
+            installingWorker.onstatechange = () => {
+              switch (installingWorker.state) {
+                case 'installed':
+                  if (!navigator.serviceWorker.controller && toastActions) {
+                    toastActions.showToast({
+                      message: '{$ cachingComplete $}',
+                    });
+                  }
+                  break;
+                case 'redundant':
+                  throw Error('The installing service worker became redundant.');
+              }
             };
-          })
-          .catch((e) => {
-            // eslint-disable-next-line no-console
-            console.error('Service worker registration failed:', e);
-          });
-      }
+          };
+        })
+        .catch((e) => {
+          // eslint-disable-next-line no-console
+          console.error('Service worker registration failed:', e);
+        });
     }
   };
 
