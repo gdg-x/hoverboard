1de1c45bd9c902f826b2d7e602cbaee873a44199
Kjell Otto EXT <info@kjell-otto.de>
2019-10-04 22:16:39 +0200

    added sessions in settings, imported data, added DANGER to runscript

--- internals/import-default-data.js
+++ internals/import-default-data.js
@@ -283,17 +283,17 @@ const importNotificationsConfig = async () => {
 };
 
 initializeFirebase()
-  .then(() => importBlog())
-  .then(() => importGallery())
-  .then(() => importNotificationsConfig())
-  .then(() => importPartners())
-  .then(() => importPreviousSpeakers())
-  .then(() => importSchedule())
+  // .then(() => importBlog())
+  // .then(() => importGallery())
+  // .then(() => importNotificationsConfig())
+  // .then(() => importPartners())
+  // .then(() => importPreviousSpeakers())
+  // .then(() => importSchedule())
   .then(() => importSessions())
-  .then(() => importSpeakers())
-  .then(() => importTeam())
-  .then(() => importTickets())
-  .then(() => importVideos())
+  // .then(() => importSpeakers())
+  // .then(() => importTeam())
+  // .then(() => importTickets())
+  // .then(() => importVideos())
 
   .then(() => {
     console.log('Finished');
