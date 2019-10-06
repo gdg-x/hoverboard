21ecd4a504c352149ca8c711efc8a83ebc6005f2
Beth Dickerson <bethdickerson17@gmail.com>
2019-10-06 12:11:51 -0400

    previous speakers archive updated to site

--- scripts/redux/actions.js
+++ scripts/redux/actions.js
@@ -332,34 +332,31 @@ const previousSpeakersActions = {
     dispatch({
       type: FETCH_PREVIOUS_SPEAKERS,
     });
-
-    firebase.firestore()
-      .collection('previousSpeakers')
-      .orderBy('order', 'asc')
-      .get()
-      .then((snaps) => {
-        const list = snaps.docs
-          .map((snap) => Object.assign({}, snap.data(), { id: snap.id }));
-
-        const obj = list.reduce(
-          (acc, curr) => Object.assign({}, acc, { [curr.id]: curr }),
-          {}
-        );
-
-        dispatch({
-          type: FETCH_PREVIOUS_SPEAKERS_SUCCESS,
-          payload: {
-            obj,
-            list,
-          },
-        });
-      })
-      .catch((error) => {
-        dispatch({
-          type: FETCH_PREVIOUS_SPEAKERS_FAILURE,
-          payload: { error },
-        });
-      });
+    return new Promise(function (resolve) {
+        fetch('data/posts/speakers-past.json')
+            .then(function (response) {
+            return response.json();
+            })
+            .then(function (res) {
+              let speakers = res.previousSpeakers;
+              const obj = speakers.reduce((acc, curr) =>
+              Object.assign({}, acc, { [curr.id]: curr }), {});
+              dispatch({
+                  type: FETCH_PREVIOUS_SPEAKERS_SUCCESS,
+                  payload: {
+                      obj,
+                      list: speakers,
+                  },
+              });
+              resolve(speakers);
+            })
+            .catch((error) => {
+              dispatch({
+                type: FETCH_PREVIOUS_SPEAKERS_FAILURE,
+                payload: { error },
+              });
+            });
+    });
   },
 };
 
