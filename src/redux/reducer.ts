9731d51865c901c2ca85254dd9a6db3f4044c758
Abraham Williams <4braham@gmail.com>
2020-09-14 20:45:54 -0500

    Switch to combineReducers

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,9 +1,9 @@
+import { combineReducers } from 'redux';
 import { blogReducer } from './blog/reducers';
 import { dialogsReducer } from './dialogs/reducers';
 import { feedbackReducer } from './feedback/reducers';
 import { filtersReducer } from './filters/reducers';
 import { galleryReducer } from './gallery/reducers';
-import { initialState } from './initial-state';
 import { notificationsReducer } from './notifications/reducers';
 import { partnersReducer } from './partners/reducers';
 import { previousSpeakersReducer } from './previous-speakers/reducers';
@@ -19,26 +19,24 @@ import { uiReducer } from './ui/reducers';
 import { userReducer } from './user/reducers';
 import { videosReducer } from './videos/reducers';
 
-export const appReducer = (state = initialState, action) => {
-  return {
-    ui: uiReducer(state.ui, action),
-    routing: routingReducer(state.routing, action),
-    dialogs: dialogsReducer(state.dialogs, action),
-    tickets: ticketsReducer(state.tickets, action),
-    partners: partnersReducer(state.partners, action),
-    feedback: feedbackReducer(state.feedback, action),
-    videos: videosReducer(state.videos, action),
-    blog: blogReducer(state.blog, action),
-    speakers: speakersReducer(state.speakers, action),
-    previousSpeakers: previousSpeakersReducer(state.previousSpeakers, action),
-    sessions: sessionsReducer(state.sessions, action),
-    schedule: scheduleReducer(state.schedule, action),
-    gallery: galleryReducer(state.gallery, action),
-    team: teamReducer(state.team, action),
-    user: userReducer(state.user, action),
-    subscribed: subscribeReducer(state.subscribed, action),
-    toast: toastReducer(state.toast, action),
-    notifications: notificationsReducer(state.notifications, action),
-    filters: filtersReducer(state.filters, action),
-  };
-};
+export const appReducer = combineReducers({
+  blog: blogReducer,
+  dialogs: dialogsReducer,
+  feedback: feedbackReducer,
+  filters: filtersReducer,
+  gallery: galleryReducer,
+  notifications: notificationsReducer,
+  partners: partnersReducer,
+  previousSpeakers: previousSpeakersReducer,
+  routing: routingReducer,
+  schedule: scheduleReducer,
+  sessions: sessionsReducer,
+  speakers: speakersReducer,
+  subscribed: subscribeReducer,
+  team: teamReducer,
+  tickets: ticketsReducer,
+  toast: toastReducer,
+  ui: uiReducer,
+  user: userReducer,
+  videos: videosReducer,
+});
