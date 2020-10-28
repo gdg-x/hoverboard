eb026a54440c5cbcc72cbbb6bd05f2ec358bfd69
Abraham Williams <4braham@gmail.com>
2020-10-27 20:59:32 -0500

    Import Firestore types

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -1,6 +1,7 @@
+import { DocumentData, QueryDocumentSnapshot } from '@firebase/firestore-types';
 import { Id } from '../models/types';
 
-export const mergeId = <T>(snapshot: firebase.firestore.QueryDocumentSnapshot): T & Id => {
+export const mergeId = <T>(snapshot: QueryDocumentSnapshot<DocumentData>): T & Id => {
   return {
     ...(snapshot.data() as T),
     ...{ id: snapshot.id },
