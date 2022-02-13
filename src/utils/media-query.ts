40bb9dbfe1983d561aeeab16726d84dab1090797
Abraham Williams <4braham@gmail.com>
2022-02-13 11:01:41 -0600

    Set isTabletPlus

--- src/utils/media-query.ts
+++ src/utils/media-query.ts
@@ -4,6 +4,7 @@ import { VIEWPORT } from '../store/ui/types';
 
 installMediaQueryWatcher(`(max-width: 639px)`, (matches) => {
   setViewportSize({ size: VIEWPORT.isPhone, matches });
+  setViewportSize({ size: VIEWPORT.isTabletPlus, matches: !matches });
 });
 
 installMediaQueryWatcher(`(min-width: 812px)`, (matches) => {
