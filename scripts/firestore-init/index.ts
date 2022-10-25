331278fae4618ca9970866c5bbf93147b5d73a64
Hugo Gresse <hugo.gresse@gmail.com>
2022-10-25 18:00:03 +0200

    Fix stuff

--- scripts/firestore-init/index.ts
+++ scripts/firestore-init/index.ts
@@ -1,13 +1,11 @@
 import { importBlog } from './blog';
-import { importConfig } from './config';
 import { importGallery } from './gallery';
 import { importPartners } from './partners';
 import { importTeam } from './team';
 import { importTickets } from './tickets';
 import { importVideos } from './videos';
 
-importConfig() // Should always be first
-  .then(() => importBlog())
+importBlog()
   .then(() => importGallery())
   .then(() => importPartners())
   .then(() => importTeam())
