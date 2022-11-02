0ca5757a43865230ce9678956f32cfcd4941018a
Abraham Williams <4braham@gmail.com>
2022-11-01 19:22:10 -0500

    Cleanup env import

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,15 +6,14 @@ import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import html from '@web/rollup-plugin-html';
 import fs from 'fs';
+import { RollupOptions } from 'rollup';
 import copy from 'rollup-plugin-copy';
 import livereload from 'rollup-plugin-livereload';
 import { generateSW } from 'rollup-plugin-workbox';
-import { compileBufferTemplate, production } from './utils/build';
+import { compileBufferTemplate, production, watch } from './utils/build';
 import { workboxConfig } from './workbox.config';
 
-const { ROLLUP_WATCH } = process.env;
-
-export default [
+const config: RollupOptions[] = [
   {
     input: 'src/firebase-messaging-sw.ts',
     treeshake: production,
@@ -73,7 +72,9 @@ export default [
       }),
       production && generateSW(workboxConfig),
       production && terser(),
-      ROLLUP_WATCH && livereload({ watch: 'dist' }),
+      watch && livereload({ watch: 'dist' }),
     ],
   },
 ];
+
+export default config;
