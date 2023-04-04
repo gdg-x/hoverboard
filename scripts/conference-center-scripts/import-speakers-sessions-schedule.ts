21c605ec647f286377751ef67fabfa4456c1d029
Hugo Gresse <hugo.gresse@gmail.com>
2023-04-04 22:20:06 +0200

    WIP

---
+++ scripts/conference-center-scripts/import-speakers-sessions-schedule.ts
@@ -0,0 +1,23 @@
+// @ts-nocheck
+import admin, { ServiceAccount } from 'firebase-admin'
+
+if (!process.env.firebaseServiceAccount) {
+  throw new Error("firebaseServiceAccount is not defined")
+}
+if (!process.env.firebaseServiceAccount.startsWith("{")) {
+  throw new Error("firebaseServiceAccount should be a JSON string")
+}
+
+if (!process.env.githubWebhookPayload || !process.env.githubWebhookPayload.startsWith("{")) {
+  throw new Error("githubWebhookPayload env missing or not a JSON")
+}
+
+const serviceAccount = JSON.parse(process.env.firebaseServiceAccount)
+
+const credential = admin.credential.cert(serviceAccount as ServiceAccount)
+admin.initializeApp({ credential })
+
+const fileContent = JSON.parse(process.env.githubWebhookPayload)
+
+console.log(fileContent)
+return
