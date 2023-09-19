bd6b7b0b5078233b95ab1e8c9cf2b536297cddd1
Josh Goldberg <git@joshuakgoldberg.com>
2023-09-19 09:53:17 -0400

    fix: most surface-level accessibility issues

---
+++ accessibility.sh
@@ -0,0 +1,4 @@
+sed -i '' 's/outline: none;/\/\* outline: none \*\//g' bower_components/*/*.html
+sed -i '' 's/outline: none;/\/\* outline: none \*\//g' bower_components/*/*/*.html
+sed -i '' 's/outline: 0;/\/\* outline: 0 \*\//g' bower_components/*/*.html
+sed -i '' 's/outline: 0;/\/\* outline: 0 \*\//g' bower_components/*/*/*.html
