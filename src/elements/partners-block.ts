a286c3d4c0bfaccad47f6be15d6af050b7af5778
Jonathan Gottfried <jonmarkgo@gmail.com>
2024-12-09 14:05:58 -0500

    fix link

--- src/elements/partners-block.ts
+++ src/elements/partners-block.ts
@@ -101,7 +101,7 @@ export class PartnersBlock extends ReduxMixin(PolymerElement) {
           </div>
         </template>
 
-        <a href="https://majorleaguehacking.typeform.com/to/nkpmOqZp">
+        <a href="https://majorleaguehacking.typeform.com/to/lVvRM8eO">
         <paper-button class="cta-button animated icon-right">
           <span>[[partnersBlock.button]]</span>
           <iron-icon icon="hoverboard:arrow-right-circle"></iron-icon>
