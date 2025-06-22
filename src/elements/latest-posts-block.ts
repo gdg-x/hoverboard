928c6c45b1ff6dd2fc2c58449df877126819a847
Abraham Williams <4braham@gmail.com>
2025-06-22 16:49:07 -0500

    Update latests-posts-block to use material/web button

--- src/elements/latest-posts-block.ts
+++ src/elements/latest-posts-block.ts
@@ -1,7 +1,7 @@
 import { Initialized, Success } from '@abraham/remotedata';
 import { computed, customElement, property } from '@polymer/decorators';
 import '@polymer/iron-icon';
-import '@polymer/paper-button';
+import '@material/web/button/text-button.js';
 import { html, PolymerElement } from '@polymer/polymer';
 import '@power-elements/lazy-image';
 import '../components/markdown/short-markdown';
@@ -116,10 +116,10 @@ export class LatestPostsBlock extends ReduxMixin(PolymerElement) {
         </div>
 
         <a href="[[latestPostsBlock.callToAction.link]]">
-          <paper-button class="cta-button animated icon-right">
+          <md-text-button class="cta-button animated icon-right">
             <span>[[latestPostsBlock.callToAction.label]]</span>
             <iron-icon icon="hoverboard:arrow-right-circle"></iron-icon>
-          </paper-button>
+          </md-text-button>
         </a>
       </div>
     `;
