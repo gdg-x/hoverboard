4cfbab26d8c390fea2caa4925e2a904504b42691
Abraham Williams <4braham@gmail.com>
2025-06-22 18:52:02 -0500

    Add mocks for web features JSDOM does not support

--- __tests__/web.setup.ts
+++ __tests__/web.setup.ts
@@ -3,3 +3,41 @@ import '@testing-library/jest-dom/jest-globals';
 
 jest.mock('firebase/messaging');
 jest.mock('../src/firebase');
+
+// @material/web uses window.matchMedia which is not available in JSDOM.
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+class MockPointerEvent {}
+
+Object.defineProperty(window, 'PointerEvent', {
+  writable: true,
+  value: MockPointerEvent,
+});
+
+Object.defineProperty(Element.prototype, 'animate', {
+  writable: true,
+  value: jest.fn().mockReturnValue({
+    cancel: jest.fn(),
+    finish: jest.fn(),
+    pause: jest.fn(),
+    play: jest.fn(),
+    reverse: jest.fn(),
+    updatePlaybackRate: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
